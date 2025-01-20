5d5b63877dcaff003949dd7f87962c1937204894
Joseph Cutor <cutormarkjoseph@gmail.com>
2025-01-20 22:57:49 +0800

    fixed token session issue, conditional middleware depending on environment

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,12 @@ import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, secureCookie: true });
-    console.log("Token:", token);
+    const isProduction = process.env.NODE_ENV === 'production';
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, secureCookie: isProduction });
     const { pathname } = req.nextUrl;
+
+    // Log the token for debugging
+    console.log("Token:", token);
 
     // Allow requests if the token exists or the path is for authentication
     if (token || pathname.includes('/signin') || pathname.includes('/api/auth')) {
